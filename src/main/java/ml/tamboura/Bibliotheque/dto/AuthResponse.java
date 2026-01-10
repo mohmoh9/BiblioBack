a22f6da2bb47e132e8b4c4abcfd9ef3c303bbda7
mohmoh9 <moskytamb69@gmail.com>
2026-01-10 01:57:27 +0000

    up  Dto

--- src/main/java/ml/tamboura/Bibliotheque/dto/AuthResponse.java
+++ src/main/java/ml/tamboura/Bibliotheque/dto/AuthResponse.java
@@ -1,4 +1,21 @@
 package ml.tamboura.Bibliotheque.dto;
 
 public class AuthResponse {
+
+    private String token;
+
+    // Constructeurs
+    public AuthResponse() {}
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
